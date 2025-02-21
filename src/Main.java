ba468d52947fc6ca6cb86e3285bc3e4b8e25962e
juliek2024 <julie.koubova.pm@gmail.com>
2025-02-21 22:03:53 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Price totalCost = new Price();
+        int distance = (int) Math.ceil (23.5);
+        System.out.println("K platbě: " + totalCost.getTotalPrice(distance) + " Kč.");
     }
 }
