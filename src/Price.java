0997091d93e42688f9153c76f1559cd50b0843e2
juliek2024 <julie.koubova.pm@gmail.com>
2025-02-21 22:04:28 +0100

    Update Price.java

--- src/Price.java
+++ src/Price.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
 public class Price {
+    private int rate;
+    private int distance;
+    private int totalPrice;
+
+    public Price() {
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getTotalPrice(int distance) {
+        setDistance(distance);
+        this.rate = getRatePerDistance();
+        this.totalPrice = distance * this.rate;
+        return totalPrice;
+    }
+
+    public int getRatePerDistance() {
+        if (distance == 0 ) {
+            rate = 0;
+        } else if (distance > 0 && distance <= 19) {
+            rate = 25;
+        } else if (distance >= 20 && distance <= 39) {
+            rate = 23;
+        } else if (distance >= 40 && distance <= 59) {
+            rate = 20;
+        } else if (distance >= 60) {
+            rate = 17;
+        } else{
+            throw new IllegalArgumentException("Zkotroluj zadanou hodnotu - vzdálenost nesmí být záporná.");
+        }
+        return rate;
+    }
 }
